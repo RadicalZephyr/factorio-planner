c5d493ee5dbe021f3716dbeacb5eec38d68999f0
Geoff Shannon <geoffpshannon@gmail.com>
2018-10-27 18:25:17 -0700

    Slightly change json format to be more decodable

--- all_recipes.lua
+++ all_recipes.lua
@@ -1,22 +1,23 @@
 local list = {}
 for _, recipe in pairs(game.player.force.recipes) do
-   item = "\"" .. recipe.name .. "\": { \"energy\": " .. recipe.energy
-   item = item .. ", \"products\": ["
+   item = "{\"name\":\"" .. recipe.name .. "\""
+   item = item .. ",\"energy\":" .. recipe.energy
+   item = item .. ",\"products\":["
    sep = ""
    for _, product in pairs(recipe.products) do
       if product.amount ~= nil then
-         item = item .. sep .. "{\"amount\": " .. product.amount .. ", \"name\": \"" .. product.name .. "\"}"
+         item = item .. sep .. "{\"amount\":" .. product.amount .. ",\"name\":\"" .. product.name .. "\"}"
          sep = ", "
       end
    end
    item = item .. " ],"
-   item = item .. "\"ingredients\": ["
+   item = item .. "\"ingredients\":["
    sep = ""
    for _, ingredient in pairs(recipe.ingredients) do
-      item = item .. sep .. "{ \"amount\": " .. ingredient.amount .. ", \"name\": \"" .. ingredient.name .. "\"}"
+      item = item .. sep .. "{\"amount\":" .. ingredient.amount .. ",\"name\":\"" .. ingredient.name .. "\"}"
       sep = ", "
    end
    item = item .. " ] }"
    table.insert(list, item)
 end
-game.write_file("recipes.json", "{" .. table.concat(list, ",\n") .. "}\n", true)
+game.write_file("recipes.json", "[" .. table.concat(list, ",") .. "]", true)
