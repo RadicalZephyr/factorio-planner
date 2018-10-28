03123e82f2d0fac5435e51a7bd6125d12f5e6161
Geoff Shannon <geoffpshannon@gmail.com>
2018-10-27 18:30:44 -0700

    Simplify extract data script

--- bin/extract_data.sh
+++ bin/extract_data.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-cat ~/.factorio/script-output/recipes.json | sed 's/\n/\n/g' > recipes-out.json
-
-rm ~/.factorio/script-output/recipes.json
+mv -v ~/.factorio/script-output/recipes.json recipes-out.json
 
 jq -c '.' < recipes-out.json > recipes.min.json
 jq    '.' < recipes-out.json > recipes.json
+
 rm recipes-out.json
