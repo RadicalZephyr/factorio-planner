c2b02b1fc0d172f9bdde025d9827f88dff47820d
Geoff Shannon <geoffpshannon@gmail.com>
2018-10-27 17:48:12 -0700

    Add a basic browser app skeleton

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,75 @@
-type alias Ingredient =
-    { name: String
-    , amount: Int
+import Browser
+import Html exposing (..)
+import Html.Events exposing (..)
+
+-- MAIN
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
     }
 
+
+-- MODEL
+
+
+type alias Model =
+  {
+  }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( Model
+  , Cmd.none
+  )
+
+
+-- UPDATE
+
+
+type Msg
+  = Dummy
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+      Dummy ->
+        ( model
+        , Cmd.none
+        )
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [ text "Hello World" ]
+    ]
+
+type alias Ingredient =
+  { name: String
+  , amount: Int
+  }
+
 type alias Recipe =
-    { name : String
-    , energy: Int
-    , products: List Ingredient
-    , ingredients : List Ingredient
-    }
+  { name : String
+  , energy: Int
+  , products: List Ingredient
+  , ingredients : List Ingredient
+  }
